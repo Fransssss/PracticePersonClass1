f375ed123edf2b379d26107e22ebe82807bd0b4c
Frans <97081479+Fransssss@users.noreply.github.com>
2022-05-27 01:34:50 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
-
+// Title: practice create a simple program with a class 
+// Purpose: practice dynamically memory, set value, and deallocate allocated memory
+// Author : Fransiskus Agapa
+// Have fun = enjoy the process - Practices make improvement
 
 int main()
 {
